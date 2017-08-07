d1710fa6e43a986c4360347c15acc4ce65cd62b9
Omar Shaikh <oshaikh13@gmail.com>
2017-08-07 17:08:54 -0400

    Added ShadowCasting Added Shadowcasting to head cube

--- Assets/Scripts/VRPawn.cs
+++ Assets/Scripts/VRPawn.cs
@@ -24,7 +24,7 @@ public class VRPawn : NetworkBehaviour {
 			CameraRig.hasChanged = false;
             // GetComponentInChildren<SteamVR_ControllerManager>().enabled = true;
             // GetComponentsInChildren<SteamVR_TrackedObject>(true).ToList().ForEach(x => x.enabled = true);
-            Head.GetComponentsInChildren<MeshRenderer>(true).ToList().ForEach(x => x.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly);
+			Head.GetComponentsInChildren<MeshRenderer>(true).ToList().ForEach(x => x.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On);
             gameObject.name = "VRPawn (LocalPlayer)";
 
 			CameraRigHead = CameraRig.transform.GetChild (2);
