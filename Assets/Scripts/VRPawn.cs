d09574c3b0e5cde832679df7bbb94fa7fb36e1d2
Omar Shaikh <oshaikh13@gmail.com>
2017-08-07 18:29:46 -0400

    Revert to original childtransforms

--- Assets/Scripts/VRPawn.cs
+++ Assets/Scripts/VRPawn.cs
@@ -64,24 +64,9 @@ public class VRPawn : NetworkBehaviour {
 			FollowObject (Head, CameraRigHead);
 			FollowObject (LeftController, CameraRigLeft);
 			FollowObject (RightController, CameraRigRight);
-
-			CmdCameraRigChange (CameraRigHead, CameraRigLeft, CameraRigRight);
-
 		}
 
 
 	}
-
-	[Command]
-	public void CmdCameraRigChange(Transform head, Transform left, Transform right) {
-		RpcCameraRigChange(head, left, right);
-	}
-
-	[ClientRpc]
-	void RpcCameraRigChange(Transform head, Transform left, Transform right) {
-		FollowObject (Head, head);
-		FollowObject (LeftController, left);
-		FollowObject (RightController, right);	
-	}
-
+		
 }
