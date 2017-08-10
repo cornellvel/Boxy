6b4544e1cda62bd75458a1ff4fce37d0cdc56f23
Omar Shaikh <oshaikh13@gmail.com>
2017-08-10 13:18:35 -0400

    Use SyncLists

--- Assets/Scripts/AutomationController.cs
+++ Assets/Scripts/AutomationController.cs
@@ -10,39 +10,33 @@ public class AutomationController : NetworkBehaviour {
 
 	private SteamVR_TrackedController controller;
 
+	SyncListString playersPressed = new SyncListString();
 
 	private void Start() {
 		// controller = GetComponent<SteamVR_TrackedController>();
 		controller = GameObject.Find("[CameraRig]").transform.GetChild(0).GetComponent<SteamVR_TrackedController>();
 		controller.TriggerClicked += HandleTriggerClicked;
+		playersPressed.Callback = PlayersPressedChanged;
 
 	}
 		
 
 	void HandleTriggerClicked (object sender, ClickedEventArgs e) {
+		
 		Debug.Log("trigger clicked");
 		buttonPressed = true;
 
-		if (!audioStarted) CmdButtonPressed (isServer);
-		// networking
-	}
 
-	[Command]
-	public void CmdButtonPressed(bool sentByServer) {
-		RpcButtonPressed (sentByServer);
-	}
-
-	[ClientRpc]
-	void RpcButtonPressed(bool sentByServer) {
-
-		// ooh this is complicated
-		if ((sentByServer && !isServer) || (!sentByServer && isServer) && buttonPressed && !audioStarted) {
-			audioStarted = true;
-			print ("starting audio!");
-			CmdButtonPressed (!sentByServer);
+		if (!playersPressed.Contains (EnvVariables.DisplayName)) {
+			playersPressed.Add (EnvVariables.DisplayName);
 		}
 
 	}
+
+	void PlayersPressedChanged(SyncListString.Operation op, int itemIndex) {
+		print ("clicked by " + playersPressed [itemIndex]);
+	}
+
 		
 	// client reception
 
