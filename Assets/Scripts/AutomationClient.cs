76f4d7c2b21b4af726aeb096013e97c8407597d1
Omar Shaikh <oshaikh13@gmail.com>
2017-08-11 10:18:54 -0400

    Create static class to handle checking 2 players

---
+++ Assets/Scripts/AutomationClient.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class AutomationClient : NetworkBehaviour {
+
+	private SteamVR_TrackedController controller;
+
+	[SyncVar(hook="ButtonPressChanged")]
+	private bool buttonPressed = false;
+
+
+	private void Start() {
+		// controller = GetComponent<SteamVR_TrackedController>();
+		controller = GameObject.Find("[CameraRig]").transform.GetChild(0).GetComponent<SteamVR_TrackedController>();
+		controller.TriggerClicked += HandleTriggerClicked;
+
+	}
+
+
+	void HandleTriggerClicked (object sender, ClickedEventArgs e) {
+
+		if (!isLocalPlayer)
+			return;
+		
+		CmdPlayerPressed ();
+
+	}
+
+	void ButtonPressChanged(bool newValue) {
+		AutomationController.addPlayersReady (EnvVariables.DisplayName);
+		buttonPressed = newValue;
+	}
+
+
+	[Command]
+	void CmdPlayerPressed() {
+		buttonPressed = true;
+	}
+
+}
--- Assets/Scripts/AutomationController.cs
+++ Assets/Scripts/AutomationController.cs
@@ -3,49 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class AutomationController : NetworkBehaviour {
+public static class AutomationController {
 
-	private SteamVR_TrackedController controller;
+	public static List<string> playersPressed = new List<string>();
 
-	private SyncListString playersPressed = new SyncListString();
+	public static bool addPlayersReady (string displayName) {
+		if (playersPressed.Contains (displayName))
+			return false;
 
-	private void Start() {
-		// controller = GetComponent<SteamVR_TrackedController>();
-		controller = GameObject.Find("[CameraRig]").transform.GetChild(0).GetComponent<SteamVR_TrackedController>();
-		controller.TriggerClicked += HandleTriggerClicked;
-		playersPressed.Callback = PlayersPressedChanged;
+		playersPressed.Add (displayName);
 
+		checkAudioStart ();
+
+		return true;
 	}
-		
-	void addPlayerPressedItem (string displayName) {
-		if (!playersPressed.Contains (displayName)) {
-			playersPressed.Add (displayName);
+
+	private static void checkAudioStart () {
+		Debug.Log (playersPressed.Count);
+		if (playersPressed.Count == 2) {
+			Debug.Log("FINALLY, DAMMIT");
 		}
-	}
-
-	void HandleTriggerClicked (object sender, ClickedEventArgs e) {
-
-		if (!isLocalPlayer)
-			return;
-		
-		if (isServer) {
-			addPlayerPressedItem (EnvVariables.DisplayName);
-		} else {
-			CmdPlayerPressed (EnvVariables.DisplayName);
-		}
-	}
-
-	void PlayersPressedChanged(SyncListString.Operation op, int itemIndex) {
-		print ("clicked by " + playersPressed [itemIndex]);
-		if (playersPressed.Count == 2 /* we're running two people */) {
-			GameObject.Find ("InstructAudio").GetComponent<AudioSource> ().Play ();
-		}
-	}
-
-
-	[Command]
-	void CmdPlayerPressed(string displayName) {
-		addPlayerPressedItem (displayName);
 	}
 
 }
