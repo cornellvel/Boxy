24473df8f54116ac8eb702d063c7ce19c005d251
Omar Shaikh <oshaikh13@gmail.com>
2017-08-24 13:10:09 -0400

    work a bit with daz

--- Assets/Scripts/VRPawn.cs
+++ Assets/Scripts/VRPawn.cs
@@ -10,14 +10,21 @@ public class VRPawn : NetworkBehaviour {
     public Transform LeftController;
     public Transform RightController;
 
-	public Transform FinalIKHead;
-	public Transform FinalIKLeft;
-	public Transform FinalIKRight;
+    public bool isDazAvatar;
+    public bool isMixamoAvatar;
+
+    public Transform DazHead;
+    public Transform DazHips;
+
+    public Vector3 adjustmentHead = new Vector3(0, 0, 0);
+    public Vector3 adjustmentHips = new Vector3(0, 0, 0);
 
 	private Transform CameraRig;
 	private Transform CameraRigHead;
 	private Transform CameraRigLeft;
 	private Transform CameraRigRight;
+
+    private float dazHeightDiff;
 
     void Start () {
         if (isLocalPlayer) { 
@@ -39,7 +46,7 @@ public class VRPawn : NetworkBehaviour {
 
         } else {
 
-			if (isRealAvatar ()) {
+			if (isMixamoAvatar) {
 				GetComponent<VRIK> ().solver.locomotion.weight = 0;
 			}
 
@@ -49,24 +56,42 @@ public class VRPawn : NetworkBehaviour {
         if (isServer && isLocalPlayer) {
             CameraRig.position = new Vector3(1, 0, 0);
             CameraRig.rotation = Quaternion.Euler(0, -90, 0);
+
         } else if (isLocalPlayer) {
             CameraRig.position = new Vector3(-1, 0, 0);
             CameraRig.rotation = Quaternion.Euler(0, 90, 0);
+
         }
 
 	}
 
+    void FollowPosition(Transform follower, Transform mover) {
+        follower.position = mover.position;
+    }
+
+    void FollowRotation(Transform follower, Transform mover) {
+        follower.rotation = mover.rotation;
+    }
+
 	void FollowObject (Transform follower, Transform mover) {
 		if (mover.hasChanged) {
 			mover.hasChanged = false;
-			follower.position = mover.position;
-			follower.rotation = mover.rotation;
+            FollowPosition(follower, mover);
+			FollowRotation(follower, mover);
 		} 
 	}
 
-	bool isRealAvatar () {
-		return FinalIKHead && FinalIKLeft && FinalIKRight;
-	}
+    void FollowDazObjects (Transform dazHips, Transform dazHead) {
+        
+        // dazHead.position = new Vector3(Head.position.x, Head.position.y, Head.position.z - .3f);
+        FollowRotation(dazHead, CameraRigHead);
+        dazHead.position = new Vector3(Head.position.x + adjustmentHead.x, dazHead.position.y + adjustmentHead.y, Head.position.z + adjustmentHead.z);
+        dazHips.position = new Vector3(Head.position.x + adjustmentHips.x, dazHips.position.y + adjustmentHips.y, Head.position.z + adjustmentHips.z);
+
+        
+        // dazHips.rotation = Quaternion.Euler(0, dazHead.rotation.eulerAngles.y, 0);
+    }
+
 
 	void Update () {
 		// This follows the CameraRig on a Transform -- if we teleport, we want the puppet to follow
@@ -74,6 +99,7 @@ public class VRPawn : NetworkBehaviour {
 			print("CameraRig Transform Change -- Updating the Puppet");
 			CameraRig.hasChanged = false;
 			this.transform.position = CameraRig.position;
+            this.transform.rotation = CameraRig.rotation;
 		}
 
 		if (isLocalPlayer) {
@@ -81,6 +107,8 @@ public class VRPawn : NetworkBehaviour {
 			FollowObject (LeftController, CameraRigLeft);
 			FollowObject (RightController, CameraRigRight);
 		}
+
+        if (isDazAvatar) FollowDazObjects(DazHips, DazHead);
 			
 	}
 		
